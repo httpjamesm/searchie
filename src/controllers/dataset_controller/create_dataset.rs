71068e02989d01e3ed339bc4d5202147bc0e8833
httpjamesm <github@httpjames.space>
2024-12-08 20:55:19 -0500

    feat: auto create world on dataset creation

--- src/controllers/dataset_controller/create_dataset.rs
+++ src/controllers/dataset_controller/create_dataset.rs
@@ -1,5 +1,9 @@
 use super::DatasetController;
 use anyhow::Result;
+use small_world_rs::{
+    distance_metric::{CosineDistance, DistanceMetric},
+    world::world::World,
+};
 impl DatasetController {
     pub async fn create_dataset(&self, name: &str) -> Result<String> {
         // force name to be lowercase
@@ -7,6 +11,17 @@ impl DatasetController {
             .dataset_repository
             .create_dataset(name.to_lowercase().as_str())
             .await?;
+
+        // create the world file for the dataset
+        let world = World::new(24, 50, 40, DistanceMetric::Cosine(CosineDistance)).unwrap();
+        // dump to file
+        std::fs::write(
+            &format!("indices/{}.smallworld", dataset_id),
+            world.dump().unwrap(),
+        )
+        .unwrap();
+        self.worlds.lock().await.insert(dataset_id.clone(), world);
+
         Ok(dataset_id)
     }
 }
